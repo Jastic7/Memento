cc4125432705b0e2c75197fbb74313b2f72f0e02
Andrey Morozov <jastic7@yandex.ru>
2017-05-04 01:26:09 +0300

    initial commit

---
+++ Memento/Memento/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Memento
+//
+//  Created by Andrey Morozov on 04.05.17.
+//  Copyright © 2017 Andrey Morozov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
