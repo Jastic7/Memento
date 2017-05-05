5933e8c4726183c7023a79cdcd9b11a8f9a6b1f9
Andrey Morozov <jastic7@yandex.ru>
2017-05-05 23:27:36 +0300

    add scrolling to overlapping rows

--- Memento/Memento/CreateSetViewController.m
+++ Memento/Memento/CreateSetViewController.m
@@ -20,6 +20,8 @@ static NSString * const kItemOfSetCellID = @"ItemOfSetTableViewCell";
 @property (nonatomic, weak) IBOutlet UITableView *tableView;
 @property (nonatomic, strong) NSMutableArray<ItemOfSet *> *items;
 @property (nonatomic, assign) NSUInteger countOfTerms;
+@property (nonatomic, copy) NSIndexPath *editingRow;
+@property (nonatomic, assign) CGSize keyboardSize;
 
 @end
 
@@ -35,6 +37,7 @@ static NSString * const kItemOfSetCellID = @"ItemOfSetTableViewCell";
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self registerForKeyboardNofifications];
     
     self.tableView.dataSource = self;
     self.tableView.estimatedRowHeight = 100;
@@ -69,6 +72,13 @@ static NSString * const kItemOfSetCellID = @"ItemOfSetTableViewCell";
     [self.tableView endUpdates];
 }
 
+-(void)textViewDidBeginEditing:(UITextView *)textView {
+    self.editingRow = [self.tableView indexPathForSubview:textView];
+    UIEdgeInsets contentInsets = UIEdgeInsetsMake(0.0, 0.0, self.keyboardSize.height, 0.0);
+    self.tableView.contentInset = contentInsets;
+    [self.tableView scrollToRowAtIndexPath:self.editingRow atScrollPosition:UITableViewScrollPositionBottom animated:true];
+}
+
 -(void)textViewDidEndEditing:(UITextView *)textView {
     NSIndexPath *indexPath = [self.tableView indexPathForSubview:textView];
     ItemOfSet *item = self.items[indexPath.row];
@@ -87,9 +97,20 @@ static NSString * const kItemOfSetCellID = @"ItemOfSetTableViewCell";
     self.delegate = nil;
 }
 
+//TODO - endEditing before check empty textFields
+
 - (IBAction)doneBarButtonTapped:(UIBarButtonItem *)sender {
     NSString *title = self.setTitleTextField.text;
     NSString *author = @"Jastic7";
+    
+    if ([title isEqualToString:@""]) {
+        [self showAlertWithTitle:@"Error" message:@"Title of the new set can't be empty."];
+        return;
+    }
+    
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"term != %@ && definition != %@", @"", @""];
+    [self.items filterUsingPredicate:predicate];
+    
     Set *set = [Set setWithTitle:title author:author items:self.items];
     
     [self.delegate saveNewSet:set];
@@ -105,5 +126,53 @@ static NSString * const kItemOfSetCellID = @"ItemOfSetTableViewCell";
     [self.tableView reloadData];
 }
 
+-(void)showAlertWithTitle:(NSString *)title message:(NSString *)message {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:title
+                                                                   message:message
+                                                            preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *action = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleCancel handler:nil];
+    [alert addAction:action];
+    
+    [self presentViewController:alert animated:YES completion:nil];
+}
+
+#pragma mark - KeyboardNotifications;
+
+-(void)registerForKeyboardNofifications {
+    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
+    
+    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(keyboardWasShown:) name:UIKeyboardDidShowNotification object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillBeHidden:) name:UIKeyboardWillHideNotification object:nil];
+}
+-(void)keyboardWillShow:(NSNotification *)notification {
+    NSDictionary *info = notification.userInfo;
+    self.keyboardSize = [[info objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+}
+
+-(void)keyboardWasShown:(NSNotification *)notification {
+    NSDictionary *info = notification.userInfo;
+    CGSize keyboardSize = [[info objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    NSLog(@"DID SHOW SIZE: %f", keyboardSize.height);
+    
+    UIEdgeInsets contentInsets = UIEdgeInsetsMake(0.0, 0.0, keyboardSize.height, 0.0);
+    self.tableView.contentInset = contentInsets;
+    self.tableView.scrollIndicatorInsets = contentInsets;
+    
+    CGRect rect = self.view.frame;
+    rect.size.height -= keyboardSize.height;
+    
+    ItemOfSetTableViewCell *cell = [self.tableView cellForRowAtIndexPath:self.editingRow];
+    if (!CGRectContainsPoint(rect, cell.frame.origin)) {
+        [((UIScrollView *)self.tableView) scrollRectToVisible:cell.frame animated:YES];
+    }
+}
+
+- (void)keyboardWillBeHidden:(NSNotification*)aNotification
+{
+    UIEdgeInsets contentInsets = UIEdgeInsetsZero;
+    self.tableView.contentInset = contentInsets;
+    self.tableView.scrollIndicatorInsets = contentInsets;
+}
 
 @end
